d8cf2e746dde0713c540e4ec2476894b0eb1253d
Ashley Pereira <ashleypereira70@gmail.com>
2019-01-08 15:36:09 +0000

    Add the TV show title overlay on hover-over #5

--- src/App.js
+++ src/App.js
@@ -15,25 +15,42 @@ class App extends Component {
         <br /> <br /> <br />
         <div className='container'>
           <div className='item'>
-          <h1>Black Mirror</h1>
             <img src={blackmirror} alt='Black Mirror' />
+            <div className='overlay'>
+              <h1>Black Mirror</h1>
+            </div>
           </div>
-          <div className='item'><h1>Breaking Bad</h1>
+          <div className='item'>
             <img src={breakingbad} alt='Breaking Bad' />
+            <div className='overlay'>
+              <h1>Breaking Bad</h1>
+            </div>
           </div>
-          <div className='item'><h1>Death Note</h1>
+          <div className='item'>
             <img src={deathnote} alt='Death Note' />
+            <div className='overlay'>
+              <h1>Death Note</h1>
+            </div>
           </div>
         </div>
         <div className='container'>
-          <div className='item'><h1>Game of Thorns</h1>
+          <div className='item'>
             <img src={gameofthrones} alt='Game of Thrones' />
+            <div className='overlay'>
+              <h1>Game of Thornes</h1>
+            </div>
           </div>
-          <div className='item'><h1>The Walking Dead</h1>
+          <div className='item'>
             <img src={thewalkingdead} alt='The walking dead' />
+            <div className='overlay'>
+              <h1>The Walking Dead</h1>
+            </div>
           </div>
-          <div className='item'><h1>The Wire</h1>
+          <div className='item'>
             <img src={thewire} alt='The Wire' />
+            <div className='overlay'>
+              <h1>The Wire</h1>
+            </div>
           </div>
         </div>
       </div>
