2e7b5d034120f52b61df185e242c59fbb9f65431
Ashley Pereira <ashleypereira70@gmail.com>
2019-01-08 12:08:58 +0000

    Added Image Folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import jamesbond from './James Bond.jpg';
+import jamesbond from './Images/James Bond.jpg';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +7,17 @@ class App extends Component {
     return (
       <div className="App">
         <img src={jamesbond} alt='James Bond'/>
+        <br/> <br/> <br/>
+      <div className='container'>
+      <div className='item'><h1>Black Mirror</h1></div>
+      <div className='item'><h1>Breaking Bad</h1></div>
+      <div className='item'><h1>Death Note</h1></div>
+      </div>
+      <div className='container'>
+      <div className='item'><h1>Game of Thorns</h1></div>
+      <div className='item'><h1>The Walking Dead</h1></div>
+      <div className='item'><h1>The Wire</h1></div>
+      </div>
       </div>
     );
   }
