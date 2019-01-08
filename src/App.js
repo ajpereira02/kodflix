937892b73e6c46d3588c36be91701001d9a7ae67
Ashley Pereira <ashleypereira70@gmail.com>
2019-01-08 13:45:28 +0000

    #2 Added immages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
-import jamesbond from './Images/James Bond.jpg';
+import blackmirror from './Images/Black Mirror.png';
+import breakingbad from './Images/Breaking Bad.jpg';
+import deathnote from './Images/Death Note.jpg';
+import gameofthrones from './Images/Game of Thrones.jpg';
+import thewalkingdead from './Images/The walking dead.jpg';
+import thewire from './Images/The Wire.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={jamesbond} alt='James Bond'/>
-        <br/> <br/> <br/>
-      <div className='container'>
-      <div className='item'><h1>Black Mirror</h1></div>
-      <div className='item'><h1>Breaking Bad</h1></div>
-      <div className='item'><h1>Death Note</h1></div>
-      </div>
-      <div className='container'>
-      <div className='item'><h1>Game of Thorns</h1></div>
-      <div className='item'><h1>The Walking Dead</h1></div>
-      <div className='item'><h1>The Wire</h1></div>
-      </div>
+
+        <br /> <br /> <br />
+        <div className='container'>
+          <div className='item'>
+          <h1>Black Mirror</h1>
+            <img src={blackmirror} alt='Black Mirror' />
+          </div>
+          <div className='item'><h1>Breaking Bad</h1>
+            <img src={breakingbad} alt='Breaking Bad' />
+          </div>
+          <div className='item'><h1>Death Note</h1>
+            <img src={deathnote} alt='Death Note' />
+          </div>
+        </div>
+        <div className='container'>
+          <div className='item'><h1>Game of Thorns</h1>
+            <img src={gameofthrones} alt='Game of Thrones' />
+          </div>
+          <div className='item'><h1>The Walking Dead</h1>
+            <img src={thewalkingdead} alt='The walking dead' />
+          </div>
+          <div className='item'><h1>The Wire</h1>
+            <img src={thewire} alt='The Wire' />
+          </div>
+        </div>
       </div>
     );
   }
