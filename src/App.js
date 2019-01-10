ae0ef68dde1134f345af9d9653f0240b4a9ce372
Ashley Pereira <ashleypereira70@gmail.com>
2019-01-10 14:41:04 +0000

    Create a Gallery component to show the covers #9

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React, { Component } from 'react';
-import TVShow from './TVShow';
-import blackmirror from './Images/Black Mirror.png';
-import breakingbad from './Images/Breaking Bad.jpg';
-import deathnote from './Images/Death Note.jpg';
-import gameofthrones from './Images/Game of Thrones.jpg';
-import thewalkingdead from './Images/The walking dead.jpg';
-import thewire from './Images/The Wire.png';
-import './App.css';
+import Gallery from './Gallery';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <br /> <br /> <br />
-        <div className='container'>
-          <TVShow name='Black Mirror' logo={blackmirror} />
-          <TVShow name='Breaking Bad' logo={breakingbad} />
-          <TVShow name='Death Note' logo={deathnote} />
-        </div>
-        <div className='container'>
-          <TVShow name='Game of Thrones' logo={gameofthrones} />
-          <TVShow name='The Walking Dead' logo={thewalkingdead} />
-          <TVShow name='The Wire' logo={thewire} />
-        </div>
+        <Gallery />
       </div>
     );
   }
 }
+
 export default App;
---
+++ src/Gallery.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TVShow from './TVShow';
+import blackmirror from './Images/Black Mirror.png';
+import breakingbad from './Images/Breaking Bad.jpg';
+import deathnote from './Images/Death Note.jpg';
+import gameofthrones from './Images/Game of Thrones.jpg';
+import thewalkingdead from './Images/The walking dead.jpg';
+import thewire from './Images/The Wire.png';
+import './App.css';
+
+export default function Gallery(){
+    return (
+      <div>
+        <div className='container'>
+          <TVShow name='Black Mirror' logo={blackmirror} />
+          <TVShow name='Breaking Bad' logo={breakingbad} />
+          <TVShow name='Death Note' logo={deathnote} />
+        </div>
+        <div className='container'>
+          <TVShow name='Game of Thrones' logo={gameofthrones} />
+          <TVShow name='The Walking Dead' logo={thewalkingdead} />
+          <TVShow name='The Wire' logo={thewire} />
+        </div>
+      </div>
+    )
+  }
