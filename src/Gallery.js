150a61c80af3227b3a88ba3e288341ec6243711d
Ashley Pereira <ashleypereira70@gmail.com>
2019-01-11 13:03:09 +0000

    #10 Added a "-" in between the spaces of the id name

--- src/Gallery.js
+++ src/Gallery.js
@@ -8,19 +8,19 @@ import thewalkingdead from './Images/The walking dead.jpg';
 import thewire from './Images/The Wire.png';
 import './App.css';
 
-export default function Gallery(){
-    return (
-      <div>
-        <div className='container'>
-          <TVShow name='Black Mirror' logo={blackmirror} />
-          <TVShow name='Breaking Bad' logo={breakingbad} />
-          <TVShow name='Death Note' logo={deathnote} />
-        </div>
-        <div className='container'>
-          <TVShow name='Game of Thrones' logo={gameofthrones} />
-          <TVShow name='The Walking Dead' logo={thewalkingdead} />
-          <TVShow name='The Wire' logo={thewire} />
-        </div>
+export default function Gallery() {
+  return (
+    <div>
+      <div className='container'>
+        <TVShow id='black-mirror' name='Black Mirror' logo={blackmirror} />
+        <TVShow id='breaking-bad' name='Breaking Bad' logo={breakingbad} />
+        <TVShow id='death-note' name='Death Note' logo={deathnote} />
       </div>
-    )
-  }
+      <div className='container'>
+        <TVShow id='game-of-thrones' name='Game of Thrones' logo={gameofthrones} />
+        <TVShow id='the-walking-dead' name='The Walking Dead' logo={thewalkingdead} />
+        <TVShow id='the-wire' name='The Wire' logo={thewire} />
+      </div>
+    </div>
+  )
+}
