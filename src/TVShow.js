efcb7f41aa5b13d2e60349dcef789d8823ce37f6
Ashley Pereira <ashleypereira70@gmail.com>
2019-01-22 11:41:44 +0000

    Removed the padding

--- src/TVShow.js
+++ src/TVShow.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function TVShow(props) {
   return (
     <Link to={`/${props.id}`} className='item'>
-      <img src={props.logo} alt={`${props.name} logo`} />
+      <img className='img' src={props.logo} alt={`${props.name} logo`} />
       <div className='overlay'>
         <h1>{props.name}</h1>
       </div>
