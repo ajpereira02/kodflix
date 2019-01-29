2e34b3555287af9816267a602fe6571b2013a953
Ashley Pereira <ashleypereira70@gmail.com>
2019-01-29 14:58:28 +0000

    Return a list of shows from the backend #19

---
+++ src/backend/tvShows.js
@@ -0,0 +1,13 @@
+
+function tvShows() {
+    return [
+        { id: 'black-mirror', name: 'Black Mirror', details: 'Black Mirror was inspired by older anthology series like The Twilight Zone, which were able to deal with controversial, contemporary topics without fear of censorship. Brooker developed Black Mirror to highlight topics related to humanity\'s relationship to technology, creating stories that feature, the way we live now – and the way we might be living in 10 minutes\' time if we\'re clumsy' },
+        { id: 'breaking-bad', name: 'Breaking Bad', details: 'A terminally ill chemistry teacher teams with a former student to manufacture crystal meth to secure his family\'s future.' },
+        { id: 'death-note', name: 'Death Note', details: 'When a Japanese high schooler comes into possession of a mystical notebook, he finds he has the power to kill anybody whose name he enters in it.' },
+        { id: 'game-of-thrones', name: 'Game of Thrones', details: 'Summers span decades. Winters can last a lifetime. And the struggle for the Iron Throne begins. Based on the bestselling book series by George R.R. Martin and created by David Benioff and D.B. Weiss.' },
+        { id: 'the-walking-dead', name: 'The Walking Dead', details: 'Based on the comic book series written by Robert Kirkman, this gritty drama portrays life in the months and years that follow a zombie apocalypse. Led by former police officer Rick Grimes, his family and a group of other survivors find themselves constantly on the move in search of a safe and secure home. But the pressure each day to stay alive sends many in the group to the deepest depths of human cruelty, and Rick discovers that the overwhelming fear of the survivors can be more deadly than the zombies walking among them.' },
+        { id: 'the-wire', name: 'The Wire', details: 'This series looks at the narcotics scene in Baltimore through the eyes of law enforcers as well as the drug dealers and users. Other facets of the city that are explored in the series are the government and bureaucracy, schools and the news media. The show was created by former police reporter David Simon, who also wrote many of the episodes.' },
+    ]
+};
+
+module.exports = tvShows;
